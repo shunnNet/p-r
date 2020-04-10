d546074604051de8aa2c8a58006e6f02b1cac865
shunnNet <wendell20904102@gmail.com>
2020-04-10 11:26:12 +0800

    pocket api imple 0.2

--- controller/pocket.js
+++ controller/pocket.js
@@ -1,79 +1,121 @@
 const pocketSet = require("../auth").pocketSet ;
 const fetch = require('node-fetch');
+const Custom = require("../controller/customUtil");
 
+// FIX : timeout and retry.
 const Pocket = {
     fetchOptionBase : {
         method : "post",
         headers : {
             "Content-Type" : "application/json; charset=UTF-8",
             "X-Accept" : "application/json"
-        }
+        },
+        timeout: 10000, // ms, node-fetch extension
     },
     url : {
         request : "https://getpocket.com/v3/oauth/request",
         authorize: "https://getpocket.com/v3/oauth/authorize",
         user_auth: "https://getpocket.com/auth/authorize",
         retrieve : "https://getpocket.com/v3/get",
-        modify : "https://getpocket.com/v3/send"
+        modify : "https://getpocket.com/v3/send",
     },
     action : {
         addTagsById : this.addTagsById
     },
-    async getReqToken(){
+    getReqToken(){
         const options = this.makeFetchOption(pocketSet);
-        return await this.fetch(this.url.request,options)
+        return this.fetch(this.url.request,options)
+                   .then(response =>{
+                       response.redirect_url = this.makeUserAuthUrl(response.code)
+                       return response
+                   })
     },
 
-    async getAccessToken(reqToken){
+    getAccessToken(reqToken){
         const body = {
             consumer_key : pocketSet.consumer_key,
             code : reqToken
         };
         
         const options = this.makeFetchOption(body);
-        return await this.fetch(this.url.authorize,options)
+        return this.fetch(this.url.authorize,options)
     },
-    async getDatas(access_token,params={}){
+    authenticate(access_token){
+        const body = {
+            consumer_key : pocketSet.consumer_key,
+            access_token : access_token,
+            state : "all",
+            tag : "random_readed_testtest"
+        };
+        
+        const options = this.makeFetchOption(body);
+        return this.fetch(this.url.retrieve,options)
+    },
+    getDatas(access_token,params={}){
         const body = {
             consumer_key : pocketSet.consumer_key,
             access_token,
             ...params
         }
         const options = this.makeFetchOption(body);
-        return await this.fetch(this.url.retrieve,options)
+        return this.fetch(this.url.retrieve,options)
+                   .then(this.reformRetriveResponse)
 
     }, 
-    async modify(access_token,actions){ // make action chainable
+    modify(access_token,actions){ // make action chainable
         const body = {
             consumer_key : pocketSet.consumer_key,
             access_token,
             actions
         };
         const options = this.makeFetchOption(body);
-        return await this.fetch(this.url.modify,options)
+        return this.fetch(this.url.modify,options)
     },
-    
 
-    async fetch(url,options){
-        return await fetch(url,options)
-                    .then(response => {
-                        var data = response.ok ? response.json() : response.text()
-                        return data.then(d => {
-                            return {
-                                ok : response.ok,
-                                status : response.status,
-                                statusText : response.statusText,
-                                data : d
-                            }
+    tag_add(access_token,item_id,tag){
+        const actions = [{
+            action : "tags_add",
+            item_id : item_id,
+            tags : tag
+        }]
+        return this.modify(access_token,actions)
+    },
+    tag_remove(access_token,item_id,tag){
+        const actions = [{
+            action : "tags_remove",
+            item_id : item_id,
+            tags : tag
+        }]
+        return this.modify(access_token,actions)
+    },
+
+    fetch(url,options){
+        console.log("api request : ")
+        console.log(url)
+        console.log(options)
+        return fetch(url,options)
+                .then(response => {
+                    console.log(response)
+                    if (!response.ok){
+                        console.trace("apiResponse")
+                        console.table({
+                            ok : response.ok,
+                            status : response.status,
+                            statusText : response.statusText,
                         })
-                    }, err=> {
-                        return {
-                            ok : false,
-                            status : 404,
-                            statusText : "newwork error",
-                            data : "network error"
-                        }
-                    })
+                    }
+                    return response.json()
+                })
+                
+    },
+    reformRetriveResponse(response) {
+        let result = [];
+        for (let id in response.list){
+            let article = response.list[id]
+            article.tags = article.tags ? Object.keys(article.tags) : [];
+            result.push(article)
+        }
+        return result
     },
 
 
@@ -87,6 +129,5 @@ const Pocket = {
         return `${this.url.user_auth}?request_token=${reqToken}&redirect_uri=${pocketSet.redirect_uri}`
     }
 };
-
 
 module.exports = Pocket;
