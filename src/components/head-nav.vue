92b0aec2061fc65706df6b2eadcb0f3323d17267
shunnNet <wendell20904102@gmail.com>
2020-04-30 19:03:29 +0800

    head-nav layout

--- src/components/head-nav.vue
+++ src/components/head-nav.vue
@@ -1,9 +1,10 @@
 <template>
   <nav class="navi">
-    <a href class="navi__logoBox">
-      <img src="../image/logo_sample.svg" alt="random" 
-            class="navi__logoBox__logo" />
-    </a>
+    <font-awesome-icon class="navi__iconBtn mg-r-050 d-none d-block-pad" icon="bars" @click="$emit('toggle_menu')"></font-awesome-icon>
+    <h1 class="navi__logoBox">
+        random
+    </h1>
+    
   </nav>
 </template>
 
@@ -16,12 +17,28 @@ export default {
 <style lang="scss">
 @import "../scss/env";
 .navi {
-    padding: 20px;
+    display: flex;
+    align-items: center;
+    background-color: $body-bg;
+    padding: 10px 20px;
+    
   &__logoBox {
-    &__logo {
       display: block;
-      height: 30px;
-    }
+      text-indent: 101%;
+      overflow: hidden;
+      background-image: url(../image/fake_logo.svg);
+      background-size: contain;
+      background-repeat: no-repeat;
+      height : 40px;
+      flex: 1;
+  }
+  &__iconBtn{
+      color: $sub;
+      user-select: none;
+      height : 40px;
+      font-size: 1.75em;
+      cursor: pointer;
+      @extend %no-pointer-touch-device;
   }
 }
 </style>
